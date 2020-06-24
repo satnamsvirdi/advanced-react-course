e555e81e5574e9d4ee0336aa791ad4a504a8851d
Satnam <satnam10virdi@gmail.com>
2020-06-24 15:22:38 +0530

    Testing JavaScript Code with Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
     "commonjs": true,
     "es6": true,
     "node": true,
+    "jest": true
   },
   "extends": ["eslint:recommended", "plugin:react/recommended"],
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
-      "jsx": true
+      "jsx": true,
     },
     "sourceType": "module"
   },
