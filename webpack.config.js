3561c1e93c0aa6de067c77d6b0948e998aaa75e0
Satnam <satnam10virdi@gmail.com>
2020-07-10 12:00:23 +0530

    Presentational and Container Components

--- webpack.config.js
+++ webpack.config.js
@@ -20,23 +20,7 @@ const config = {
                 use: {
                     loader: 'babel-loader',  
                     options: {
-                        plugins: [
-                            '@babel/plugin-proposal-class-properties',
-                            [
-                                '@babel/plugin-proposal-decorators',
-                                {
-                                  'legacy': true
-                                }
-                            ],
-                            '@babel/plugin-proposal-export-namespace-from',
-                            '@babel/plugin-proposal-function-sent',
-                            '@babel/plugin-proposal-json-strings',
-                            '@babel/plugin-proposal-numeric-separator',
-                            '@babel/plugin-proposal-throw-expressions',
-                            '@babel/plugin-syntax-dynamic-import',
-                            '@babel/plugin-syntax-import-meta',
-                            ["transform-class-properties", { "spec": true }]
-                        ]
+                        plugins: []
                     }
                 }
             }
