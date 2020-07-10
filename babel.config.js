cc9c938c04d6fdad65d5c067c2e1bb4b105eaef0
Satnam <satnam10virdi@gmail.com>
2020-07-10 12:00:23 +0530

    Presentational and Container Compoonents

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,6 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
-  	"@babel/plugin-proposal-class-properties",
-      [
-        "@babel/plugin-proposal-decorators",
-        {
-          "legacy": true
-        }
-      ],
-      "@babel/plugin-proposal-export-namespace-from",
-      "@babel/plugin-proposal-function-sent",
-      "@babel/plugin-proposal-json-strings",
-      "@babel/plugin-proposal-numeric-separator",
-      "@babel/plugin-proposal-throw-expressions",
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-syntax-import-meta",
-      ["transform-class-properties", { "spec": true }]
+  	"@babel/plugin-proposal-class-properties"
   ]
 };
